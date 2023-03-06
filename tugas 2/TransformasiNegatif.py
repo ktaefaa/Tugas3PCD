a4865c8183e1f1062cd7a59c11d4bac4f8712660
afifawulandari <96385550+ktaefaa@users.noreply.github.com>
2023-03-06 07:08:31 +0700

    Add files via upload

---
+++ tugas 2/TransformasiNegatif.py
@@ -0,0 +1,15 @@
+#NAMA : MOH ISRAN
+#NIM : F55121073
+#KELAS : B
+
+import cv2
+import numpy as np
+
+img = cv2.imread("mammogram.tif", 0)
+img_1 = 255 - img
+
+cv2.imshow("Original Image", img)
+cv2.imshow("Image Negative", img_1)
+
+cv2.waitKey(0)
+cv2.destroyAllwindows()
